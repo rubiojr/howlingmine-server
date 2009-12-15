d21956b7b92474f6c9678cf7036c76d437e2927d
Sergio Rubio <sergio@rubio.name>
2009-12-15 11:09:07 +0100

    bumb version to 0.1.1

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :howlingmine_server do
   name 'Howling Mine Server Redmin plugin'
   author 'Sergio Rubio'
   description 'Adds JSON interface to Redmine Tickets'
-  version '0.1'
+  version '0.1.1'
 end
